6281ec5603bcde85d0582def58033b586ffb14fc
Miguel Angel Ramos <return.infox@gmail.com>
2024-04-16 10:10:27 -0400

    poo javascript en base a clases

---
+++ js/app.js
@@ -0,0 +1,29 @@
+class UserManager {
+  
+  constructor() {
+    this.users = [];
+    this.currentId = 0;
+  }
+
+  //* CREATE - Crear un usuario
+  addUser(name, email) {
+
+  }
+
+  //* READ -  Leer informacion del usuario
+  getUser(id) {
+
+  }
+
+  //* UPDATED - Actualizar el usuario
+
+  updateUser(id, name, email) {
+
+  }
+
+  //* DELETE - Eliminar el usuario
+
+  deleteUser(id) {
+
+  }
+}
